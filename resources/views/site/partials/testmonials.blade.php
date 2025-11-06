b497d91b2ad8c312a51f0f86ed7de21383b3fdec
EL ARYBY AZZEDINE <aryby00@gmail.com>
2025-11-06 03:01:20 +0100

    fix testmonials lang

--- resources/views/site/partials/testmonials.blade.php
+++ resources/views/site/partials/testmonials.blade.php
@@ -13,27 +13,26 @@
             -->
   
             <div class="testimonial">
-  
-              <h2 class="title">testimonial</h2>
-  
+
+              <h2 class="title">{{ __('testimonials.what_our_clients_say') }}</h2>
+
               <div class="testimonial-card">
-  
-                <img src="/inspire/assets/images/testimonial-1.jpg" alt="alan doe" class="testimonial-banner" width="80"
+
+                <img src="/inspire/assets/images/testimonial-1.jpg" alt="satisfied customer" class="testimonial-banner" width="80"
                   height="80">
-  
-                <p class="testimonial-name">Alan Doe</p>
-  
-                <p class="testimonial-title">CEO & Founder Invision</p>
-  
+
+                <p class="testimonial-name">{{ __('testimonials.client_name') }}</p>
+
+                <p class="testimonial-title">{{ __('testimonials.client_title') }}</p>
+
                 <img src="/inspire/assets/images/icons/quotes.svg" alt="quotation" class="quotation-img" width="26">
-  
+
                 <p class="testimonial-desc">
-                  Lorem ipsum dolor sit amet consectetur Lorem ipsum
-                  dolor dolor sit amet.
+                  {{ __('testimonials.description') }}
                 </p>
-  
+
               </div>
-  
+
             </div>
   
   
@@ -48,13 +47,13 @@
   
               <a href="#" class="cta-content">
   
-                <p class="discount">25% Discount</p>
+                <p class="discount">{{ __('cta.discount') }}</p>
   
-                <h2 class="cta-title">Summer collection</h2>
+                <h2 class="cta-title">{{ __('cta.summer_collection') }}</h2>
   
-                <p class="cta-text">Starting @ $10</p>
+                <p class="cta-text">{{ __('cta.starting_at') }}</p>
   
-                <button class="cta-btn">Shop now</button>
+                <button class="cta-btn">{{ __('cta.shop_now') }}</button>
   
               </a>
   
@@ -67,88 +66,88 @@
             -->
   
             <div class="service">
-  
-              <h2 class="title">Our Services</h2>
-  
+
+              <h2 class="title">{{ __('services.our_services') }}</h2>
+
               <div class="service-container">
-  
+
                 <a href="#" class="service-item">
-  
+
                   <div class="service-icon">
                     <ion-icon name="boat-outline"></ion-icon>
                   </div>
-  
+
                   <div class="service-content">
-  
-                    <h3 class="service-title">Worldwide Delivery</h3>
-                    <p class="service-desc">For Order Over $100</p>
-  
+
+                    <h3 class="service-title">{{ __('services.delivery_morocco') }}</h3>
+                    <p class="service-desc">{{ __('services.delivery_speed_security') }}</p>
+
                   </div>
-  
+
                 </a>
-  
+
                 <a href="#" class="service-item">
-  
+
                   <div class="service-icon">
                     <ion-icon name="rocket-outline"></ion-icon>
                   </div>
-  
+
                   <div class="service-content">
-  
-                    <h3 class="service-title">Next Day delivery</h3>
-                    <p class="service-desc">UK Orders Only</p>
-  
+
+                    <h3 class="service-title">{{ __('services.cash_on_delivery') }}</h3>
+                    <p class="service-desc">{{ __('services.cash_on_delivery_description') }}</p>
+
                   </div>
-  
+
                 </a>
-  
+
                 <a href="#" class="service-item">
-  
+
                   <div class="service-icon">
                     <ion-icon name="call-outline"></ion-icon>
                   </div>
-  
+
                   <div class="service-content">
-  
-                    <h3 class="service-title">Best Online Support</h3>
-                    <p class="service-desc">Hours: 8AM - 11PM</p>
-  
+
+                    <h3 class="service-title">{{ __('services.online_support') }}</h3>
+                    <p class="service-desc">{{ __('services.support_hours') }}</p>
+
                   </div>
-  
+
                 </a>
-  
+
                 <a href="#" class="service-item">
-  
+
                   <div class="service-icon">
                     <ion-icon name="arrow-undo-outline"></ion-icon>
                   </div>
-  
+
                   <div class="service-content">
-  
-                    <h3 class="service-title">Return Policy</h3>
-                    <p class="service-desc">Easy & Free Return</p>
-  
+
+                    <h3 class="service-title">{{ __('services.return_policy') }}</h3>
+                    <p class="service-desc">{{ __('services.return_policy_description') }}</p>
+
                   </div>
-  
+
                 </a>
-  
+
                 <a href="#" class="service-item">
-  
+
                   <div class="service-icon">
                     <ion-icon name="ticket-outline"></ion-icon>
                   </div>
-  
+
                   <div class="service-content">
-  
-                    <h3 class="service-title">30% money back</h3>
-                    <p class="service-desc">For Order Over $100</p>
-  
+
+                    <h3 class="service-title">{{ __('services.money_back') }}</h3>
+                    <p class="service-desc">{{ __('services.money_back_condition') }}</p>
+
                   </div>
-  
+
                 </a>
-  
+
               </div>
-  
+
             </div>
   
           </div>
